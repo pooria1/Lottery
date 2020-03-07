76a657822e1b83cf264242631820924046a68ddd
Pooria Kalantari <poori@aut.ac.ir>
2020-03-08 01:46:30 +0330

    now we have array of string instead of array of struct

--- main.go
+++ main.go
@@ -5,24 +5,12 @@ import (
 	"math/rand"
 )
 
-type ball struct {
-	color string
-	ID    int
-}
-
-var ID int
-
-type balls []ball
-
-func (b *balls) addBall(color string) {
-	ID++
-	*b = append(*b, ball{color, ID})
-}
+type balls []string
 
 func (b *balls) getNumBalls(color string) int {
 	ans := 0
 	for _, v := range *b {
-		if v.color == color {
+		if v == color {
 			ans++
 		}
 	}
@@ -34,25 +22,19 @@ func main() {
 	var b balls
 
 	for {
-		fmt.Println("[1] Add ball\n[2] get random ball\n[3] quit")
+		fmt.Println("[1] Add ball\n[2] quit")
 		var choice int
 		fmt.Scanf("%d", &choice)
 
-		switch choice {
-
-		case 1:
-			fmt.Print("color: ")
+		if choice == 1 {
+			fmt.Print("adding a ball with color: ")
 			var c string
 			fmt.Scanf("%s", &c)
-			b.addBall(c)
+			b = append(b, c)
+			i := rand.Intn(len(b))
+			fmt.Println("Random ball is", b[i], "with", b.getNumBalls(b[i]), "points.")
 			break
-
-		case 2:
-			i := rand.Intn(ID)
-			fmt.Println("Your ball is", b[i].color, "and point is", b.getNumBalls(b[i].color))
-			break
-
-		default:
+		} else {
 			return
 		}
 	}
